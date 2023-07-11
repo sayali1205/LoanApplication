0f11809b832cf61a93312fed2b3b0fc423c38768
poojayedle <poojamyedle@gmail.com>
2023-07-11 15:55:07 +0530

    loanApp

---
+++ LoanApplication/src/test/java/com/loan/achintya/data/LoanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loan.achintya.data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
