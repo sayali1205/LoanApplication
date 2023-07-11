640195d14a3efd8daf7bcfb8f77f6ee41d5ba775
poojayedle <poojamyedle@gmail.com>
2023-07-11 16:18:35 +0530

    pojo

---
+++ LoanApplication/src/main/java/com/loan/achintya/data/Model/User.java
@@ -0,0 +1,19 @@
+package com.loan.achintya.data.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+	@Id
+	private int uid;
+	private String userName;
+
+}
