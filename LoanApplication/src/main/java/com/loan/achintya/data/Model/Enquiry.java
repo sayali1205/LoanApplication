9ac3a0fbc02b05b699e18c53607ff90ebdeba4dc
Abhijeet4545 <giriabhijeet91@gmail.com>
2023-07-12 14:14:42 +0530

    auto genrated value

--- LoanApplication/src/main/java/com/loan/achintya/data/Model/Enquiry.java
+++ LoanApplication/src/main/java/com/loan/achintya/data/Model/Enquiry.java
@@ -1,6 +1,8 @@
 package com.loan.achintya.data.Model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import lombok.AllArgsConstructor;
@@ -12,6 +14,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Enquiry {
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int custId;
 	private String custFirstName;
 	private String custLastName;
