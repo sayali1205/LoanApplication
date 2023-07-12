ea90f1ee1582a48054baf5cea4adeec0a8d323c9
JayaGunjkar <jayagunjkar4428@gmail.com>
2023-07-12 14:56:14 +0530

    address add

--- LoanApplication/src/main/java/com/loan/achintya/data/Model/BankDetails.java
+++ LoanApplication/src/main/java/com/loan/achintya/data/Model/BankDetails.java
@@ -1,5 +1,6 @@
 package com.loan.achintya.data.Model;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -11,7 +12,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-
+@Entity
 
 public class BankDetails {
 
