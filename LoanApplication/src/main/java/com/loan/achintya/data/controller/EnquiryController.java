e9b836354236e3224f56c05b5b8457c00257652a
poojayedle <poojamyedle@gmail.com>
2023-07-12 18:07:43 +0530

    ddd

--- LoanApplication/src/main/java/com/loan/achintya/data/controller/EnquiryController.java
+++ LoanApplication/src/main/java/com/loan/achintya/data/controller/EnquiryController.java
@@ -1,8 +1,13 @@
 package com.loan.achintya.data.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.loan.achintya.data.service.EnquiryService;
 
 @RestController
 public class EnquiryController {
 
+	@Autowired
+	EnquiryService enquiryService;
 }
