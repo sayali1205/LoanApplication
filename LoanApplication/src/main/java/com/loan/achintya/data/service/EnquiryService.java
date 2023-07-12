2f03ce23a10795803a1171695590b22730b8b935
sayali1205 <sayalikawarkar@gmail.com>
2023-07-12 18:05:45 +0530

    Crud Classes

---
+++ LoanApplication/src/main/java/com/loan/achintya/data/service/EnquiryService.java
@@ -0,0 +1,5 @@
+package com.loan.achintya.data.service;
+
+public interface EnquiryService {
+
+}
---
+++ LoanApplication/src/main/java/com/loan/achintya/data/serviceImpl/EnquiryServiceImpl.java
@@ -0,0 +1,10 @@
+package com.loan.achintya.data.serviceImpl;
+
+import org.springframework.stereotype.Service;
+
+import com.loan.achintya.data.service.EnquiryService;
+
+@Service
+public class EnquiryServiceImpl implements EnquiryService {
+
+}
