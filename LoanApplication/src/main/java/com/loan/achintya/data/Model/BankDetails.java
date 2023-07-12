09b43215bd70350aeb05c094385b0e25b2d7744b
JayaGunjkar <jayagunjkar4428@gmail.com>
2023-07-12 14:34:07 +0530

    changes in bank details

--- LoanApplication/src/main/java/com/loan/achintya/data/Model/BankDetails.java
+++ LoanApplication/src/main/java/com/loan/achintya/data/Model/BankDetails.java
@@ -1,6 +1,15 @@
 package com.loan.achintya.data.Model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 
 public class BankDetails {
 
+	private int BankId;
 }
