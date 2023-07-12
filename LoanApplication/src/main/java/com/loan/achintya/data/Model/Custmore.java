b5bba5f8e9c8d5b947e9cc03054f959aa337872c
poojayedle <poojamyedle@gmail.com>
2023-07-12 14:56:09 +0530

    class

---
+++ LoanApplication/src/main/java/com/loan/achintya/data/Model/Custmore.java
@@ -0,0 +1,31 @@
+package com.loan.achintya.data.Model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Custmore {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int custId;
+	private String custFirstName;
+    private String custLastName;
+    private String custGender;
+    private long custContactNo;
+    private String custEmailId;
+    private String custProfeshion;
+    @OneToOne(cascade = CascadeType.ALL)
+    private DocumentDetails docData;
+              
+}
