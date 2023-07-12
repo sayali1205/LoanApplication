fa0ded53d59a2178bd852f8698a6a8d9e6399022
akshay25031998 <akshayrajput7057@gmail.com>
2023-07-12 04:05:38 -0700

    EMICalcultor

---
+++ LoanApplication/src/main/java/com/loan/achintya/data/model/EMICalculator.java
@@ -0,0 +1,31 @@
+package com.loan.achintya.data.model;
+
+import java.util.Scanner;
+
+public class EMICalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter loan amount: ");
+        double principal = scanner.nextDouble();
+
+        System.out.print("Enter annual interest rate (in percentage): ");
+        double annualInterestRate = scanner.nextDouble();
+        double monthlyInterestRate = annualInterestRate / 12 / 100;
+
+        System.out.print("Enter loan tenure (in months): ");
+        int tenure = scanner.nextInt();
+
+        double emi = calculateEMI(principal, monthlyInterestRate, tenure);
+        System.out.println("The EMI is: " + emi);
+
+        scanner.close();
+    }
+
+    public static double calculateEMI(double principal, double monthlyInterestRate, int tenure) {
+        double numerator = principal * monthlyInterestRate * Math.pow(1 + monthlyInterestRate, tenure);
+        double denominator = Math.pow(1 + monthlyInterestRate, tenure) - 1;
+        return numerator / denominator;
+    }
+}
+
