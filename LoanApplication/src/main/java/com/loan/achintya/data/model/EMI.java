f8d828af94b1dadd44fac9761739c3a96afa05c0
akshay25031998 <akshayrajput7057@gmail.com>
2023-07-12 04:19:22 -0700

    veriable created

--- LoanApplication/src/main/java/com/loan/achintya/data/model/EMI.java
+++ LoanApplication/src/main/java/com/loan/achintya/data/model/EMI.java
@@ -1,13 +1,19 @@
 package com.loan.achintya.data.model;
 
+import javax.persistence.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class EMI {
-	 public static void main (String args[])  
-	    { 
-		 float p, r,  t,  si; // principal amount, rate, time and simple interest respectively  
-	              p = 13000;  r = 12; t = 2;  
-	               si  = (p*r*t)/100;   
-	              System.out.println("Simple Interest is: " +si);  
-	    }
+	 private double principal;
+	    private double annualInterestRate;
+	    private int tenure;
 	 }  
 
 
