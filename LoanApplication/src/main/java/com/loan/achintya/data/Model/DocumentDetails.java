afa4a79ad802474f4a36060f10af6d702c324738
poojayedle <poojamyedle@gmail.com>
2023-07-12 14:37:47 +0530

    documnetclass

--- LoanApplication/src/main/java/com/loan/achintya/data/Model/DocumentDetails.java
+++ LoanApplication/src/main/java/com/loan/achintya/data/Model/DocumentDetails.java
@@ -1,8 +1,23 @@
 package com.loan.achintya.data.Model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class DocumentDetails {
 
+	@Id
+	private int docId;
 	private byte[] pancard;
-	
+	private byte[] adharCard;
+	private byte[] form16;
+	private byte[] itr;
 	
 }
