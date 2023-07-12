a32aaf7e4636b747ad96937c259961d7f58dc778
sayali1205 <sayalikawarkar@gmail.com>
2023-07-12 18:08:40 +0530

    cls

--- LoanApplication/src/main/java/com/loan/achintya/data/serviceImpl/EnquiryServiceImpl.java
+++ LoanApplication/src/main/java/com/loan/achintya/data/serviceImpl/EnquiryServiceImpl.java
@@ -1,10 +1,14 @@
 package com.loan.achintya.data.serviceImpl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.loan.achintya.data.repository.EnquiryRepository;
 import com.loan.achintya.data.service.EnquiryService;
 
 @Service
 public class EnquiryServiceImpl implements EnquiryService {
+	@Autowired
+	EnquiryRepository enquiryRepository;
 
 }
