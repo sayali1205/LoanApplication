1faaa62ae2b32aceec245a7f6ceb02ff8aa5ed29
JayaGunjkar <jayagunjkar4428@gmail.com>
2023-07-13 22:17:54 +0530

    ...

--- LoanApplication/src/main/java/com/loan/achintya/data/model/EMI.java
+++ LoanApplication/src/main/java/com/loan/achintya/data/model/EMI.java
@@ -1,9 +1,6 @@
 package com.loan.achintya.data.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
