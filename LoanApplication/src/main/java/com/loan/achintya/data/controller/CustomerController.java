c74000ff056cb30f66e0a302e7be9a0dd276f8f7
JayaGunjkar <jayagunjkar4428@gmail.com>
2023-07-18 15:23:27 +0530

    SaveCustAPI

--- LoanApplication/src/main/java/com/loan/achintya/data/controller/CustomerController.java
+++ LoanApplication/src/main/java/com/loan/achintya/data/controller/CustomerController.java
@@ -1,14 +1,20 @@
 package com.loan.achintya.data.controller;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.loan.achintya.data.model.Customer;
 import com.loan.achintya.data.service.CustomerService;
 
@@ -20,10 +26,25 @@ public class CustomerController {
 	
 	CustomerService cs;
 	
-	@PostMapping("/saveCust")
-	public Customer saveCustomer(@RequestBody Customer cust)
+	@PostMapping(value="/saveCust",produces = MediaType.APPLICATION_JSON_VALUE)
+	public Customer saveCustomer(@RequestPart(value="pancard")MultipartFile pan,
+			@RequestPart(value="adharCard")MultipartFile adhar,
+			@RequestPart(value="photo")MultipartFile photo,
+			@RequestPart(value="signature")MultipartFile signature,
+			@RequestPart(value="form16")MultipartFile form16,
+			@RequestPart(value="itr")MultipartFile itr,
+			@RequestPart(value="data") String json ) throws Exception
 	{
-		return cs.saveCustomer(cust) ;
+		
+		ObjectMapper om=new ObjectMapper();
+		Customer c=om.readValue(json, Customer.class);
+		c.getDocData().setPancard(pan.getBytes());
+		c.getDocData().setAdharCard(adhar.getBytes());
+		c.getDocData().setPhoto(photo.getBytes());
+		c.getDocData().setSignature(signature.getBytes());
+		c.getDocData().setForm16(form16.getBytes());
+		c.getDocData().setItr(itr.getBytes());
+		return cs.saveCustomer(c) ;
 	}
 	
 	@GetMapping("/getAllCustomer")
